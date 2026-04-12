c7adca48e8c295aba756ab2e59ed34663d7832dc
sandeep10704 <saivenkatasandeep3@gmail.com>
2026-04-12 16:50:19 +0530

    add corcs

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # or ["*"] for testing
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
